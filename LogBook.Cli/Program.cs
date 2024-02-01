3224037cbcecbab9e82afef75ff864f0eec3b54d
William Nisbet <wnisbet@schueler.hakzell.at>
2024-02-01 13:08:00 +0100

    Add Demodata

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -3,6 +3,10 @@
 Console.WriteLine("Wilkommen beim Fahrtenbuch");
 
 Irepository repository = new MemoryRepository("logbook.xml");
+repository.Add(new Entry(DateTime.Now,DateTime.Now.AddHours(2).AddMinutes(22), 25000,25180,"ZE-XY123", "Zell am See", "München"));
+Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(2), DateTime.Now.AddDays(2).AddMinutes(20), 25500, 25514, "ZE-XY123", "Zell am See", "Saalfelden");
+repository.Add(entrySaalfelden);
+
 List<Entry> entries = repository.GetAll();
 
 foreach (Entry entry in entries )
