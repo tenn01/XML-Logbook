3e67cee92dc143176e296fa8a4b71bcb2816d270
William Nisbet <wnisbet@schueler.hakzell.at>
2024-02-05 12:18:25 +0100

    create add, remove,save methods

--- LogBook.Lib/xmlRepository.cs
+++ LogBook.Lib/xmlRepository.cs
@@ -9,22 +9,71 @@ namespace LogBook.Lib
 {
     public class xmlRepository : Irepository
     {
-        XElement _root;
-        
+        XElement _rootElement;
+
+        private string _path;
+
+        public xmlRepository(string path)
+        {
+            _path = path;
+            if (File.Exists(path))
+            {
+                _rootElement = XElement.Load(path);
+            }
+            else
+            {
+                _rootElement = new XElement("root");
+            }
+        }
 
         public bool Add(Entry entry)
         {
-            throw new NotImplementedException();
+            XElement node = new XElement("entry");
+
+            var idAtrib = new XAttribute("id", entry.ID.ToString());
+            node.Add(idAtrib);
+
+            var startAtrib = new XAttribute("start", entry.Start.ToString());
+            node.Add(startAtrib);
+
+            var endAtrib = new XAttribute("end", entry.End.ToString());
+            node.Add(endAtrib);
+
+            var startkmAtrib = new XAttribute("startkm", entry.StartKM.ToString());
+            node.Add(startkmAtrib);
+
+            var endkmAtrib = new XAttribute("endkm", entry.EndKM.ToString());
+            node.Add(endkmAtrib);
+
+            var numberplateAtrib = new XAttribute("numberplate", entry.NumberPlate.ToString());
+            node.Add(numberplateAtrib);
+
+            var fromAtrib = new XAttribute("from", entry.From.ToString());
+            node.Add(fromAtrib);
+
+            var toAtrib = new XAttribute("to", entry.To.ToString());
+            node.Add(toAtrib);
+
+            node.Add(entry.Description.ToString());
+
+            _rootElement.Add(node);
+
+            return this.Save();
+
         }
 
         public bool Delete(Entry entry)
         {
-            throw new NotImplementedException();
+            var itemsDel = from i in _rootElement.Descendants("entry")
+                           where (string)i.Attribute("id") == entry.ID
+                           select i;
+            itemsDel.Remove();
+            return this.Save();
         }
 
         public List<Entry> GetAll()
         {
-            var entries = from entry in _root.Descendants("entry")
+            var entries = from entry in _rootElement.Descendants("entry")
                           select entry;
             throw new NotImplementedException();
             // return entries.ToList<Entry>();
@@ -35,7 +84,16 @@ namespace LogBook.Lib
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            try
+            {
+                _rootElement.Save(_path);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         public bool Update(Entry entry)
@@ -43,16 +101,6 @@ namespace LogBook.Lib
             throw new NotImplementedException();
         }
 
-        public xmlRepository(string path)
-        {
-         if (File.Exists(path))
-            {
-                _root = XElement.Load(path);
-            }
-         else
-            {
-                _root = new XElement("root");
-            }
-        }
+        
     }
 }
