84612626bdf196298dd285c33669024308e29c76
William Nisbet <wnisbet@schueler.hakzell.at>
2024-02-05 12:21:53 +0100

    include use xml repository in programm

--- LogBook.Cli/Program.cs
+++ LogBook.Cli/Program.cs
@@ -2,8 +2,12 @@
 
 Console.WriteLine("Wilkommen beim Fahrtenbuch");
 
-Irepository repository = new MemoryRepository("logbook.xml");
+string path = "logbook.xml";
+
+Irepository repository = new xmlRepository(path);
+
 repository.Add(new Entry(DateTime.Now,DateTime.Now.AddHours(2).AddMinutes(22), 25000,25180,"ZE-XY123", "Zell am See", "München"));
+
 Entry entrySaalfelden = new Entry(DateTime.Now.AddDays(2), DateTime.Now.AddDays(2).AddMinutes(20), 25500, 25514, "ZE-XY123", "Zell am See", "Saalfelden");
 repository.Add(entrySaalfelden);
 
