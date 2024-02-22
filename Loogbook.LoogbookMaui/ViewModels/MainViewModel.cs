9b13289aaf5999a110c6444cbc1e1f8a59a0beed
David Miska <dmiska@schueler.hakzell.at>
2024-02-22 08:33:23 +0100

    Fahrten werden ausgegeben (CollectionView inkl. Trmplate)

--- Loogbook.LoogbookMaui/ViewModels/MainViewModel.cs
+++ Loogbook.LoogbookMaui/ViewModels/MainViewModel.cs
@@ -4,6 +4,7 @@ using LogBook.Lib;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,24 @@ namespace Loogbook.LoogbookMaui.ViewModel
             foreach(var entry in entries)
             {
                 Entries.Add(entry);
+            }
+        }
+
+        [RelayCommand]
+        void Add()
+        {
+            LogBook.Lib.Entry entrySaalfelden = new(
+                DateTime.Now.AddDays(3),
+                DateTime.Now.AddDays(3).AddMinutes(20), 25500, 25514,
+                "Ze-XY123", "Zell am See", "Saalfelden")
+            {
+                Description = "Fahrt nach Saalfelden"
+            };
+
+            var result = _repository.Add(entrySaalfelden);
+            if(result)
+            {
+                this.Entries.Add(entrySaalfelden);
             }
         }
     }
