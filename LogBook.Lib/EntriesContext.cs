ad697b9297588bd4d1bbe543aec79ed1d8c9938d
tenn01 <tenn@schueler.hakzell.at>
2024-04-04 07:40:27 +0200

    Sqlite Database

---
+++ LogBook.Lib/EntriesContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBook.Lib;
+
+public class EntriesContext : DbContext
+{
+    public DbSet<Entry> Entries { get; set; }
+    
+    private string _path = string.Empty;
+
+    public EntriesContext(string path)
+    {
+        _path = path;
+        SQLitePCL.Batteries_V2.Init();
+        this.Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        Debug.WriteLine(_path);
+
+        optionsBuilder.UseSqlite($"Filename={_path}");   
+        
+        // base.OnConfiguring(optionsBuilder);
+    }
+
+
+}
---
+++ LogBook.Lib/Sqliterepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogBook.Lib;
+
+public class Sqliterepository : Irepository
+{
+    private string _path = String.Empty;
+
+    public Sqliterepository(string path)
+    {
+        _path = path;
+    }
+
+    public bool Add(Entry entry)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(Entry entry)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Entry? Find(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Entry> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Save()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Update(Entry entry)
+    {
+        throw new NotImplementedException();
+    }
+}
